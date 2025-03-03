da253157b20a8acf4881798d09a099dd5987339f
jagriti <119938056+Jagriti143@users.noreply.github.com>
2025-03-03 18:03:21 +0530

    Time: 20 ms (78.85%), Space: 55.7 MB (77.14%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class Solution {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+
+        }
+        return nums;
+    }
+}
