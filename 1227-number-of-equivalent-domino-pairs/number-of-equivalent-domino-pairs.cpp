8470f17a4917b210a91fe9e38b302c13e34def8f
jagriti <119938056+Jagriti143@users.noreply.github.com>
2025-05-18 18:42:01 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (71.99%) - LeetSync

---
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        unordered_map<int, int> freq;
+        int count = 0;
+
+        for (auto& d : dominoes) {
+            int a = min(d[0], d[1]);
+            int b = max(d[0], d[1]);
+            int key = a * 10 + b; 
+
+            count += freq[key]; 
+            freq[key]++;        
+        }
+
+        return count;
+    }
+};
+
