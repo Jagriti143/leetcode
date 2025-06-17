e72c9c1791d266958121359195c7ee656931913a
jagriti <119938056+Jagriti143@users.noreply.github.com>
2025-06-17 13:42:52 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (58.04%) - LeetSync

--- 3747-maximum-difference-between-adjacent-elements-in-a-circular-array/maximum-difference-between-adjacent-elements-in-a-circular-array.cpp
+++ 3747-maximum-difference-between-adjacent-elements-in-a-circular-array/maximum-difference-between-adjacent-elements-in-a-circular-array.cpp
@@ -4,10 +4,10 @@ public:
        int n=nums.size();
        int maxi=abs(nums[n-1]-nums[0]);
        for(int i=0;i<n-1;i++){
-        if(abs(nums[i]-nums[i+1]) >maxi){
+        /*if(abs(nums[i]-nums[i+1]) >maxi){
             maxi=abs(nums[i]-nums[i+1]);
-        }
-        //maxi=max(maxi,abs(nums[i]-nums[i+1]));
+        }*/
+        maxi=max(maxi,abs(nums[i]-nums[i+1]));
        } 
        return maxi;
     }
