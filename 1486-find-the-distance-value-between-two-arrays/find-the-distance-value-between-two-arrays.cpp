d01a5574b2066d254800e57d889fcc6e108738fb
jagriti <119938056+Jagriti143@users.noreply.github.com>
2025-07-09 13:41:59 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (14.32%) - LeetSync

--- 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.cpp
+++ 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.cpp
@@ -5,33 +5,35 @@ public:
         int n=arr2.size();
         int cnt=0;
         
-        //sort(arr2.begin(),arr2.end());
+        sort(arr2.begin(),arr2.end());
         for(int i=0;i<m;i++){
              bool flag=true;
-            // int left=0;
-            // int right=n-1;
-            // while(left<=right){
+            int left=0;
+            int right=n-1;
+            while(left<=right){
                 
-            //     int mid=left+(right-left)/2;
-            //     if(abs(arr1[i]-arr2[mid])<=d){
-            //         flag=false;
-
-            //         break;
-            //     }else{
-            //         right=mid-1;
-            //     }
-            // }
-            // if(flag) cnt++;
-            for(int j=0;j<n;j++){
-                if(abs(arr1[i]-arr2[j])<=d){
+                int mid=left+(right-left)/2;
+                if(abs(arr1[i]-arr2[mid])<=d){
                     flag=false;
-                    break;
-                }
 
+                    break;
+                }else if(arr1[i]>arr2[mid]){
+                    left=mid+1;
+                    }else{
+                    right=mid-1;
+                }
             }
-            if(flag){
-                cnt++;
-            }
+            if(flag) cnt++;
+            // for(int j=0;j<n;j++){
+            //     if(abs(arr1[i]-arr2[j])<=d){
+            //         flag=false;
+            //         break;
+            //     }
+
+            // }
+            // if(flag){
+            //     cnt++;
+            // }
         }
         return cnt;
     }
