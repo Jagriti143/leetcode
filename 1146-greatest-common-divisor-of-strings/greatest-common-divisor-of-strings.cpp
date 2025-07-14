dbff5fee3856bb8e7db077b6043fd46f46a2dfd0
jagriti <119938056+Jagriti143@users.noreply.github.com>
2025-07-14 10:36:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (30.89%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -3,10 +3,8 @@ public:
     string gcdOfStrings(string str1, string str2) {
         int m=str1.size();
         int n=str2.size();
-        //string s="";
         if (str1 + str2 != str2 + str1)
             return "";
-        //s += str1;
         int gcd_len = gcd(m, n);
         return str1.substr(0, gcd_len);
         
