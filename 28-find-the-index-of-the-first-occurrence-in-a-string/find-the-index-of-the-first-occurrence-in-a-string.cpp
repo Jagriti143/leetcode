392b327eec7addf21b60c814b1a2a4a463a4772a
jagriti <119938056+Jagriti143@users.noreply.github.com>
2025-07-15 11:08:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (59.10%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -6,19 +6,6 @@ public:
         if(n==0){
             return 0;
         }
-        // if(m==1 && n==1){
-        //     if(haystack[0]==needle[0]){
-        //         return 0;
-        //     }
-        // }
-        // for(int i=0;i<m;i++){
-        //     if(m>0 && n==1){
-        //         if(haystack[i]==needle[0]){
-        //             return i;
-        //         }
-        //     }
-        // }
-        
         for(int i=0;i<=m-n;++i){
             if(haystack.substr(i,n)==needle){
                 return i;
