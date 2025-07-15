82097d205b418ec0906cfbe825f4a5f561f28e1b
jagriti <119938056+Jagriti143@users.noreply.github.com>
2025-07-15 14:12:02 +0530

    Time: 7 ms (20.68%) | Memory: 9.5 MB (98.70%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -6,19 +6,32 @@ public:
         if(m!=n){
             return false;
         }
-        map<char,int> mp;
-        map<char,int> p;
+        // map<char,int> mp;
+        // map<char,int> p;
         
-        for(int i=0;i<m;i++){
-            mp[s[i]]++;
-        }
-        for(int i=0;i<n;i++){
-            p[t[i]]++;
-        }
-        for(int i=0;i<n;i++){
-            if(mp.find(t[i])==mp.end() ||  mp[t[i]]!=p[t[i]]){
+        // for(int i=0;i<m;i++){
+        //     mp[s[i]]++;
+        // }
+        // for(int i=0;i<n;i++){
+        //     p[t[i]]++;
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(mp.find(t[i])==mp.end() ||  mp[t[i]]!=p[t[i]]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        int l=0;
+        int r=0;
+        while(l<m && r<n){
+            if(s[l]!=t[r]){
+
                 return false;
             }
+            l++;
+            r++;
         }
         return true;
     }
