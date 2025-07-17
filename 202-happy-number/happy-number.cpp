68065953c3a7f23156dca9b0835eedd9c80cd95d
jagriti <119938056+Jagriti143@users.noreply.github.com>
2025-07-17 09:35:46 +0530

    Time: 3 ms (19.61%) | Memory: 7.9 MB (74.95%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+       
+     while (n != 1 && n != 4) {  
+        int sum = 0;
+        int temp = n;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += pow(digit, 2);
+            temp /= 10;
+        }
+        n = sum;
+    }
+    return n == 1;
+    }
+};
