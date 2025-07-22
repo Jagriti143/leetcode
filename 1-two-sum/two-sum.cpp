cbdd310dd5fb943074f71ac4b5a8ff6706f5e82b
jagriti <119938056+Jagriti143@users.noreply.github.com>
2025-07-22 22:07:01 +0530

    Time: 71 ms (16.97%) | Memory: 14 MB (93.04%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -5,12 +5,15 @@ public:
         for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
                 if(nums[i]+nums[j]==target){
-                    arr.push_back(i);
+                 arr.push_back(i);
                     arr.push_back(j);
                     return arr;
                 }
             }
-        }
+        }       
         return arr;
+        
+
+
     }
 };
